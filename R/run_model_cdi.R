bba8b79916122de24caae19ed0b60e6394f090f0
esthervankleef <esther.vankleef@lshtm.ac.uk>
2017-03-02 17:17:06 +0100

    Removed bits to run sensitivity analysis as this not used for lancet ID publication

--- R/run_model_cdi.R
+++ R/run_model_cdi.R
@@ -3,11 +3,6 @@ source("./R/model_functions.R")
 library(lhs)
 script.name.cdi = "hosp_com_cdi"
 
-sens.values = read.csv("./Sensitivity_values/Values_sens_cdi.csv",sep=";")
-sens.values = sens.values[!is.na(sens.values$fh),]
-
-names(sens.values) = c("fh","betaA_hcw2p","betaA2","betaB_hcw2p","betaB2","R0A","R0B","baseA","baseB")
-
 # Parameter values
 betaA1.hcw2p <- .21 #patient S to A transmission rate (from hcw to patient) in setting 1 etc
 betaA1.p2hcw <- 2.1#hcw S to A transmission rate (from patient to hcw) in setting 1 etc
@@ -62,50 +57,35 @@ R0Betc
 
 par(mfrow=c(1,1))
 manipulate(
-  plot.model(S.betaA1.hcw2p,S.betaA1.p2hcw ,S.betaA2,S.betaA3,S.betaB1.hcw2p, S.betaB1.p2hcw,S.betaB2,S.betaB3, S.hhfreq, S.time.int,S.newhhfreq,time=40000),
-  S.betaB2=slider(0,0.02,0.00443,step=0.00001),
-  S.betaB3=slider(0,0.02,0.00443,step=0.00001),
-  S.newhhfreq=slider(0,0.99,0.4,step=0.01),
-  S.betaA1.hcw2p=slider(0,.5,0.218,step=.001),
-  S.betaA1.p2hcw=slider(0,5,2.18,step=.01), 
-  S.betaA2=slider(0,0.1,.00212,step=.00001),
-  S.betaA3=slider(0,0.1,.00212,step=.00001),
-  S.betaB1.hcw2p=slider(0,.4,.19,step=.001),
-  S.betaB1.p2hcw=slider(0,4,1.9,step=0.01),
+  plot.model(S.betaA1.hcw2p,S.betaA1.p2hcw ,S.betaA2,S.betaA3,S.betaB1.hcw2p, S.betaB1.p2hcw,S.betaB2,S.betaB3, S.hhfreq, S.time.int,S.newhhfreq,time=60000),
+  S.betaA1.hcw2p=slider(0,.5,0.21,step=.001),
+  S.betaA1.p2hcw=slider(0,5,2.1,step=.01), 
+  S.betaA2=slider(0,0.1,.0031,step=.00001),
+  S.betaA3=slider(0,0.1,.0031,step=.00001),
+  S.betaB1.hcw2p=slider(0,.4,.15,step=.001),
+  S.betaB1.p2hcw=slider(0,4,1.5,step=0.01),
+  S.betaB2=slider(0,0.02,0.00515,step=0.00001),
+  S.betaB3=slider(0,0.02,0.00515,step=0.00001),
+  S.newhhfreq=slider(0,0.99,0.5,step=0.01),
   S.hhfreq=slider(0,0.99,.4,step=0.01),
-  S.time.int=slider(100,30000,25000,step=100)
+  S.time.int=slider(100,50000,45000,step=100)
 ) 
 
 par(mfrow=c(1,1))
 manipulate(
   plot.model.inc(S.betaA1.hcw2p,S.betaA1.p2hcw,S.betaA2,S.betaA3,S.betaB1.hcw2p, S.betaB1.p2hcw,S.betaB2,S.betaB3, S.hhfreq,S.time.int, S.newhhfreq,time=60000),
-  S.betaB2=slider(0,0.02,0.0055,step=0.00001),
-  S.betaB3=slider(0,0.02,0.0055,step=0.00001),
-  S.newhhfreq=slider(0,0.99,0.4,step=0.01),
   S.betaA1.hcw2p=slider(0,.5,0.21,step=.001),
   S.betaA1.p2hcw=slider(0,5,2.1,step=.01), 
   S.betaA2=slider(0,0.1,.0031,step=.00001),
   S.betaA3=slider(0,0.1,.0031,step=.00001),
-  S.betaB1.hcw2p=slider(0,.4,.14,step=.001),
-  S.betaB1.p2hcw=slider(0,4,1.4,step=0.01),
+  S.betaB1.hcw2p=slider(0,.4,.15,step=.001),
+  S.betaB1.p2hcw=slider(0,4,1.5,step=0.01),
+  S.betaB2=slider(0,0.02,0.00515,step=0.00001),
+  S.betaB3=slider(0,0.02,0.00515,step=0.00001),
+  S.newhhfreq=slider(0,0.99,0.5,step=0.01),
   S.hhfreq=slider(0,0.99,.4,step=0.01),
-  S.time.int=slider(100,30000,25000,step=100)
+  S.time.int=slider(100,50000,45000,step=100)
 ) 
-
-# Fraction of patients in community
-par(mfrow=c(1,1))
-manipulate(
-  f.trans(S.betaA1.hcw2p,S.betaA1.p2hcw,S.betaA2,S.betaA3,S.betaB1.hcw2p,betaB1.p2hcw,S.betaB2,S.betaB3,time=40000),
-  S.betaA1.hcw2p=slider(0,.5,0.195,step=.001),
-  S.betaA1.p2hcw=slider(0,5,1.95,step=.01), 
-  S.betaA2=slider(0,0.1,.00145,step=.0001),
-  S.betaA3=slider(0,0.1,.00145,step=.0001),
-  S.betaB1.hcw2p=slider(0,.1,.05,step=.001),
-  S.betaB1.p2hcw=slider(0,1,.5,step=0.01),
-  S.betaB2=slider(0,0.02,0.0026,step=0.0001),
-  S.betaB3=slider(0,0.02,0.0026,step=0.0001)
-)
-
 
 newhhfreq.vec<-c(.2,.25,.3,.35,.4,.45, 0.475, 0.5,.55) # 
 
@@ -136,7 +116,7 @@ for(i in 1:length(newhhfreq.vec)){
   modpars <- c(betaA1.hcw2p, betaA1.p2hcw,betaB1.hcw2p,betaB1.p2hcw,betaA2,betaB2,betaA3,betaB3,betaAB1.hcw2p,betaBA1.hcw2p,betaAB2,betaBA2,betaAB3,betaBA3,muA1,muB1,muA2,muB2,muA3,muB3,mS1,mA1,mB1,rho, n.hcw,hhfreq,c, time.int,newhhfreq,f23,f32)   
   mod.init <-c(S1.t0,A1.t0,B1.t0,S2.t0,A2.t0,B2.t0,S3.t0,A3.t0,B3.t0,H.A1.t0,H.B1.t0,total.t0)
   mod.t <- seq(0,730000,by=10) 
-  mod.sol <- lsoda(mod.init,mod.t,modCDI.dyn,modpars)
+  mod.sol <- lsoda(mod.init,mod.t,mod.dyn,modpars)
   N1<-S1.t0+A1.t0+B1.t0
   N2<-S2.t0+A2.t0+B2.t0
   N3<-S3.t0+A3.t0+B3.t0
@@ -178,55 +158,9 @@ for(i in 1:length(newhhfreq.vec)){
   
 } 
 
-# Run model to find correct values for sensitivity analysis
-time = 30000
-time.int = time-365
-lhs = randomLHS(1,2)
-
-lhs[,1] = c(0.228)
-lhs[,2] = c(0.00025)
-out = data.frame(matrix(nrow=1, ncol=6))
-R0.find = find.R0(lhs,out,time,time.int,"A",hhfreq = 0.4) 
-
-
-# Run sensitivity analysis
-t=730000
-time.int<-365000
-sets = length(sens.values[,1])
-sets2=length(unique(sens.values$betaB_hcw2p[!is.na(sens.values$betaB_hcw2p)]))
-
-sens.out = frac.h.sens(sets,sets2,sens.values,t,time.int,newhhfreq.vec = 0.475)
-sens.out
-
-# Run sensitivity analysis with different values bacterial inference
-t=730000
-time.int<-365000
-sets = length(sens.values[,1])
-sets2=length(unique(sens.values$betaB_hcw2p[!is.na(sens.values$betaB_hcw2p)]))
-
-sens.out.comp = frac.h.sens(sets,sets2,sens.values,t,time.int,newhhfreq.vec = 0.5)
-sens.out.comp
-
-# Check single admission numbers
-sets = length(sens.values[,1])
-sets2=length(unique(sens.values$betaB_hcw2p[!is.na(sens.values$betaB_hcw2p)]))
-single.aR0 = single.adm.r0(sets,sets2,sens.values)
-single.aR0
-
-single.aR0$h_higher = ifelse(single.aR0$RA1.1>single.aR0$RB1.1,1,0)
-
 # Save dataset
-savename <- paste0("./Output/",script.name.cdi,".Rda")
+savename <- paste0("./Output/",script.name.cdi,".Rda") # Save this in a folder of your preference
 save(matA.all,matA1,matA2and3,matB.all,matB1,matB2and3,
      matIA1,matIA2and3,matIB1,matIB2and3,
      matdIA1,matdIA2and3,matdIB1,matdIB2and3, file = savename)
 
-savename.sens <- paste0("./Output/",script.name.cdi,".sens",".Rda")
-save(sens.out,file=savename.sens)
-
-savename.admR0 <- paste0("./Output/",script.name.cdi,".admR0",".Rda")
-save(single.aR0,file=savename.admR0)
-
-
-savename.sens.comp <- paste0("./Output/",script.name,".sens.comp",".Rda")
-save(sens.out.comp,file=savename.sens.comp)
