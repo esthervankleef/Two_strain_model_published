bb6e4a25e680925c0a8fc0f3cfcccd6041505e3f
esthervankleef <esther.vankleef@lshtm.ac.uk>
2017-10-08 11:45:53 +0200

    Added code to sensitivity analysis mixing and bacterial inference

--- R/run_model.R
+++ R/run_model.R
@@ -2,8 +2,9 @@ source("./R/model_functions.R")
 
 library(lhs)
 script.name = "hosp_com"
-
+  
 sens.values = read.csv("./Sensitivity_values/Values_sens_an.csv",sep=";")
+sens.values.bi.mix = read.csv("./Sensitivity_values/Values_sens_bacinf_mix.csv",sep=";")
 
 names(sens.values) = c("fh","betaA_hcw2p","betaA2","betaB_hcw2p","betaB2","R0A","R0B","baseA","baseB")
 
@@ -63,37 +64,37 @@ R0etc
 R0Betc<-calcR0B.fqr(S1.t0,A1.t0, B1.t0,S2.t0,A2.t0, B2.t0,S3.t0,A3.t0, B3.t0,mS1,mA1,mB1,rho,muB1,muB2,muB3,betaB1.hcw2p, betaB1.p2hcw,betaB2,betaB3,n.hcw,hhfreq,c,f23,f32)
 R0Betc
 
-par(mfrow=c(1,1))
-manipulate(
-  plot.model(S.betaA1.hcw2p,S.betaA1.p2hcw ,S.betaA2,S.betaA3,S.betaB1.hcw2p, S.betaB1.p2hcw,S.betaB2,S.betaB3, S.hhfreq, S.time.int,S.newhhfreq,time=60000),
-  S.betaA1.hcw2p=slider(0,.5,0.187,step=.001),
-  S.betaA1.p2hcw=slider(0,5,1.87,step=.01), 
-  S.betaA2=slider(0,0.1,.00212,step=.00001),
-  S.betaA3=slider(0,0.1,.00212,step=.00001),
-  S.betaB1.hcw2p=slider(0,.4,.1,step=.001),
-  S.betaB1.p2hcw=slider(0,4,1.0,step=0.01),
-  S.betaB2=slider(0,0.02,0.0032,step=0.00001),
-  S.betaB3=slider(0,0.02,0.0032,step=0.00001),
-  S.newhhfreq=slider(0,0.99,0.5,step=0.01),
-  S.hhfreq=slider(0,0.99,.4,step=0.01),
-  S.time.int=slider(100,50000,45000,step=100)
-) 
-
-par(mfrow=c(1,1))
-manipulate(
-  plot.model.inc(S.betaA1.hcw2p,S.betaA1.p2hcw,S.betaA2,S.betaA3,S.betaB1.hcw2p, S.betaB1.p2hcw,S.betaB2,S.betaB3, S.hhfreq,S.time.int, S.newhhfreq,time=60000),
-  S.betaA1.hcw2p=slider(0,.5,0.187,step=.001),
-  S.betaA1.p2hcw=slider(0,5,1.87,step=.01), 
-  S.betaA2=slider(0,0.1,.00212,step=.00001),
-  S.betaA3=slider(0,0.1,.00212,step=.00001),
-  S.betaB1.hcw2p=slider(0,.4,.1,step=.001),
-  S.betaB1.p2hcw=slider(0,4,1.0,step=0.01),
-  S.betaB2=slider(0,0.02,0.0032,step=0.00001),
-  S.betaB3=slider(0,0.02,0.0032,step=0.00001),
-  S.newhhfreq=slider(0,0.99,0.5,step=0.01),
-  S.hhfreq=slider(0,0.99,.4,step=0.01),
-  S.time.int=slider(100,50000,45000,step=100)
-) 
+# par(mfrow=c(1,1))
+# manipulate(
+#   plot.model(S.betaA1.hcw2p,S.betaA1.p2hcw ,S.betaA2,S.betaA3,S.betaB1.hcw2p, S.betaB1.p2hcw,S.betaB2,S.betaB3, S.hhfreq, S.time.int,S.newhhfreq,time=60000),
+#   S.betaA1.hcw2p=slider(0,.5,0.187,step=.001),
+#   S.betaA1.p2hcw=slider(0,5,1.87,step=.01), 
+#   S.betaA2=slider(0,0.1,.00212,step=.00001),
+#   S.betaA3=slider(0,0.1,.00212,step=.00001),
+#   S.betaB1.hcw2p=slider(0,.4,.1,step=.001),
+#   S.betaB1.p2hcw=slider(0,4,1.0,step=0.01),
+#   S.betaB2=slider(0,0.02,0.0032,step=0.00001),
+#   S.betaB3=slider(0,0.02,0.0032,step=0.00001),
+#   S.newhhfreq=slider(0,0.99,0.5,step=0.01),
+#   S.hhfreq=slider(0,0.99,.4,step=0.01),
+#   S.time.int=slider(100,50000,45000,step=100)
+# ) 
+# 
+# par(mfrow=c(1,1))
+# manipulate(
+#   plot.model.inc(S.betaA1.hcw2p,S.betaA1.p2hcw,S.betaA2,S.betaA3,S.betaB1.hcw2p, S.betaB1.p2hcw,S.betaB2,S.betaB3, S.hhfreq,S.time.int, S.newhhfreq,time=60000),
+#   S.betaA1.hcw2p=slider(0,.5,0.187,step=.001),
+#   S.betaA1.p2hcw=slider(0,5,1.87,step=.01), 
+#   S.betaA2=slider(0,0.1,.00212,step=.00001),
+#   S.betaA3=slider(0,0.1,.00212,step=.00001),
+#   S.betaB1.hcw2p=slider(0,.4,.1,step=.001),
+#   S.betaB1.p2hcw=slider(0,4,1.0,step=0.01),
+#   S.betaB2=slider(0,0.02,0.0032,step=0.00001),
+#   S.betaB3=slider(0,0.02,0.0032,step=0.00001),
+#   S.newhhfreq=slider(0,0.99,0.5,step=0.01),
+#   S.hhfreq=slider(0,0.99,.4,step=0.01),
+#   S.time.int=slider(100,50000,45000,step=100)
+# ) 
 
 newhhfreq.vec<-c(.2,.25,.3,.35,.4,.45, 0.5, 0.55,.6) # 
 
@@ -173,7 +174,7 @@ lhs[,2] = c(0.00234)
 out = data.frame(matrix(nrow=1, ncol=6))
 R0.find = find.R0(lhs,out,time,time.int,"B",hhfreq = 0.4) 
 
-# Run sensitivity analysis
+# Run sensitivity analysis hospital adaptation 
 t=730000
 time.int<-365000
 sets = length(sens.values[,1])
@@ -182,6 +183,82 @@ sets2=length(unique(sens.values$betaB_hcw2p[!is.na(sens.values$betaB_hcw2p)]))
 sens.out = frac.h.sens(sets,sets2,sens.values,t,time.int,newhhfreq.vec = 0.5)
 sens.out
 
+# Run sensitivity analysis bacterial inference
+sets = length(sens.values.bi.mix$bac_inf_half)
+sets2=length(unique(sens.values$betaB_hcw2p[!is.na(sens.values$betaB_hcw2p)]))
+
+sens.out = frac.h.sens(sets,sets2,sens.values,t,time.int,newhhfreq.vec = 0.5)
+sens.out
+
+# Run sensitivity analysis bacterial inference and mixing
+sens.scen<-c("bi1","bi2","mix1","mix2") # 
+l<-length(sens.scen)
+
+sensA1<-matrix(rep(NA,73001*l),ncol=l) # 73001 is time
+sensB1<-matrix(rep(NA,73001*l),ncol=l)
+sensA2and3<-matrix(rep(NA,73001*l),ncol=l)
+sensB2and3<-matrix(rep(NA,73001*l),ncol=l)
+sensA.all<-matrix(rep(NA,73001*l),ncol=l)
+sensB.all<-matrix(rep(NA,73001*l),ncol=l)
+
+sensIA1<-matrix(rep(NA,73001*l),ncol=l) # 73001 is time
+sensIB1<-matrix(rep(NA,73001*l),ncol=l)
+sensIA2and3<-matrix(rep(NA,73001*l),ncol=l)
+sensIB2and3<-matrix(rep(NA,73001*l),ncol=l)
+
+sensdIA1<-matrix(rep(NA,73001*l),ncol=l) # 73001 is time
+sensdIB1<-matrix(rep(NA,73001*l),ncol=l)
+sensdIA2and3<-matrix(rep(NA,73001*l),ncol=l)
+sensdIB2and3<-matrix(rep(NA,73001*l),ncol=l)
+
+for(i in 1:length(sens.scen)){
+  print(sens.scen[i])
+  #time.int<-365000
+  #modpars <- c(betaA1.hcw2p, betaA1.p2hcw,betaB1.hcw2p,betaB1.p2hcw,betaA2,betaB2,betaA3,betaB3,betaAB1.hcw2p,betaBA1.hcw2p,betaAB2,betaBA2,betaAB3,betaBA3,muA1,muB1,muA2,muB2,muA3,muB3,mS1,mA1,mB1,rho, n.hcw,hhfreq,c, time.int,newhhfreq,f23,f32)   
+  modpars <- sens.values.bi.mix[,2+i]
+  mod.init <-c(S1.t0,A1.t0,B1.t0,S2.t0,A2.t0,B2.t0,S3.t0,A3.t0,B3.t0,H.A1.t0,H.B1.t0,total.t0)
+  mod.t <- seq(0,730000,by=10) 
+  mod.sol <- lsoda(mod.init,mod.t,mod.dyn,modpars)
+  N1<-S1.t0+A1.t0+B1.t0
+  N2<-S2.t0+A2.t0+B2.t0
+  N3<-S3.t0+A3.t0+B3.t0
+  TIME <- mod.sol[,1] 
+  A1 <- mod.sol[,3]/N1 
+  B1 <- mod.sol[,4]/N1 
+  A2and3<- (mod.sol[,6]+mod.sol[,9])/(N2+N3)
+  B2and3<- (mod.sol[,7]+mod.sol[,10])/(N2+N3)
+  A.all<-(mod.sol[,3]+mod.sol[,6]+mod.sol[,9])/(N1+N2+N3)
+  B.all<-(mod.sol[,4]+mod.sol[,7]+mod.sol[,10])/(N1+N2+N3)
+  
+  sensA1[,i]<-A1
+  sensB1[,i]<-B1
+  sensA2and3[,i]<-A2and3
+  sensB2and3[,i]<-B2and3
+  sensA.all[,i] <- A.all
+  sensB.all[,i] <- B.all
+  
+  # INCIDENCE
+  IA1 <- mod.sol[,14] 
+  IB1 <- mod.sol[,17] 
+  IA2and3<- (mod.sol[,15]+mod.sol[,16])
+  IB2and3<- (mod.sol[,18]+mod.sol[,19])
+  sensIA1[,i]<-IA1
+  sensIB1[,i]<-IB1
+  sensIA2and3[,i]<-IA2and3
+  sensIB2and3[,i]<-IB2and3
+  
+  # INCIDENCE PER DAY
+  IdA1 <- mod.sol[,20] 
+  IdB1 <- mod.sol[,23] 
+  IdA2and3<- (mod.sol[,21]+mod.sol[,22])
+  IdB2and3<- (mod.sol[,24]+mod.sol[,25])
+  sensdIA1[,i]<-IdA1
+  sensdIB1[,i]<-IdB1
+  sensdIA2and3[,i]<-IdA2and3
+  sensdIB2and3[,i]<-IdB2and3
+} 
+
+  
 # Save dataset
 savename <- paste0("./Output/",script.name,".Rda")
 save(matA.all,matA1,matA2and3,matB.all,matB1,matB2and3,
@@ -190,4 +267,9 @@ save(matA.all,matA1,matA2and3,matB.all,matB1,matB2and3,
 
 savename.sens <- paste0("./Output/",script.name,".sens",".Rda")
 save(sens.out,file=savename.sens)
+
+savename.sens.bi.mix <- paste0("./Output/",script.name,".sens_bi_mix",".Rda")
+save(sensA.all,sensA1,sensA2and3,sensB.all,sensB1,sensB2and3,
+     sensIA1,sensIA2and3,sensIB1,sensIB2and3,
+     sensdIA1,sensdIA2and3,sensdIB1,sensdIB2and3, file = savename.sens.bi.mix)
 
